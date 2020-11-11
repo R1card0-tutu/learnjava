ac8800489b94b0c0e5eac83e545dd5597035c165
2tu <zhangzhenbeta@gmail.com>
2020-11-11 21:36:05 +0800

    learning javadoc

---
+++ day01/Hello.java
@@ -0,0 +1,37 @@
+/*
+对第一个java程序进行总结
+1.java程序编写-编译-运行的过程
+编写：我们将编写的java代码保存在以".java"结尾的源文件中
+编译：使用javac命令编译我们的java源文件。格式：javac 源文件名.java
+运行：使用java命令解释运行我们的字节码文件。格式：java 类名
+
+2.在一个java源文件可以声明多个class。但是，只能最多有一个类声明为public的。
+
+
+
+
+
+
+
+
+
+*/
+
+
+
+
+
+
+class Hello{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
+
+public class Person{
+
+}
+
+class Animal{
+
+}
